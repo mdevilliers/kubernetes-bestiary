82c1e69bdec72b01c7dc26b60b5614c0ab34e2e5
Mark deVilliers <markdevilliers@gmail.com>
2015-05-17 20:39:29 +0100

    Base64 encode/decode all of the secrets

--- secrets/web-image/src/main.go
+++ secrets/web-image/src/main.go
@@ -1,6 +1,7 @@
 package main
  
 import(
+    "encoding/base64"
 	"fmt"
 	"io/ioutil"
 	"net/http"
@@ -11,28 +12,37 @@ func indexHandler( w http.ResponseWriter, r *http.Request){
 
 	path := "/etc/foo"
 
-	username, err := ioutil.ReadFile(path + "/username")
+	usernameEncoded, err := ioutil.ReadFile(path + "/username")
 
 	if err != nil {
 		fmt.Fprintf(w, "error reading username : %s", err.Error())
+		return
 	}
 
-	password, err := ioutil.ReadFile(path + "/password")
+	username, _ := base64.StdEncoding.DecodeString(string(usernameEncoded))
+
+	passwordEncoded , err := ioutil.ReadFile(path + "/password")
 
 	if err != nil {
 		fmt.Fprintf(w, "error reading password : %s", err.Error())
+		return
 	}
 
-	other, err := ioutil.ReadFile(path + "/another-secret")
+	password, _ := base64.StdEncoding.DecodeString(string(passwordEncoded))
+
+	otherEncoded, err := ioutil.ReadFile(path + "/another-secret")
 
 	if err != nil {
 		fmt.Fprintf(w, "error reading another-secret : %s", err.Error())
+		return
 	}
+
+	other, _ := base64.StdEncoding.DecodeString(string(otherEncoded))
 
 	fmt.Fprintf(w, "ssh!, I'm running on %s. My secrets are username : %s, password : %s, other : %s", 
 		runtime.GOOS,string(username), string(password), string(other))
 }
- 
+
 func main(){
 	http.HandleFunc("/", indexHandler)
 	http.ListenAndServe(":8080",nil)
