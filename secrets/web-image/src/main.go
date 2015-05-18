dcdd7c92ef4c84cea7fe5cdb2e7387b6a61b64ab
Mark deVilliers <markdevilliers@gmail.com>
2015-05-18 11:07:36 +0100

    tweak

--- secrets/web-image/src/main.go
+++ secrets/web-image/src/main.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"encoding/base64"
 	"fmt"
 	"io/ioutil"
 	"net/http"
@@ -27,20 +26,14 @@ func loadSecret(w http.ResponseWriter, key string) {
 
 	path := "/etc/foo/"
 
-	encoded, err := ioutil.ReadFile(path + key)
+	secret, err := ioutil.ReadFile(path + key)
 
 	if err != nil {
 		fmt.Fprintf(w, "error reading %s : %s", key, err.Error())
 		return
 	}
-	unencoded, err := base64.StdEncoding.DecodeString(string(encoded))
 
-	if err != nil {
-		fmt.Fprintf(w, "error decoding %s : %s", string(encoded), err.Error())
-		return
-	}
-
-	fmt.Fprintf(w, "<br/>ssh!, Key : %s, Secret : %s from : %s", key, unencoded, string(encoded))
+	fmt.Fprintf(w, "<br/>ssh!, Key : %s, Secret : %s", key, secret)
 
 }
 
